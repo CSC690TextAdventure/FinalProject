28f9a37f7ea75e8a28892c9f30c920a285d2ecb6
kentosfsu <32149707+kentosfsu@users.noreply.github.com>
2018-05-15 14:51:04 -0700

    added four buttons for moving rooms

--- LibraryEscape/RoomView.swift
+++ LibraryEscape/RoomView.swift
@@ -3,7 +3,9 @@ import UIKit
 
 public class RoomView : UIView {
     
+    var options : [OptionLabel] = (1...4).map {_ in OptionLabel()}
     var delegate : RoomViewDelegate?
+    var textField : TextLabel = TextLabel()
     let room : Room
     
     init(_ room : Room) {
@@ -14,21 +16,80 @@ public class RoomView : UIView {
         layer.borderColor = UIColor.white.cgColor
         layer.borderWidth = 3.0
         let textField : RoomNameLabel = RoomNameLabel(room.roomName)//
-        addSubview(textField)    }
+        addSubview(textField)
+        options.forEach {option in addSubview(option)}
+        alignLabels()
+        displayOptions()
+    }
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError()
     }
     
-
-//    func displayText(_ text : String) {
-//        textField2.text = text
-//    }
     
-    func didTapButton(button: UILabel) {
+    //    func displayText(_ text : String) {
+    //        textField2.text = text
+    //    }
+    
+    //need changed for correct positions
+    func alignLabels() {
+        textField.align(.top)
+        textField.align(.leading)
+        textField.align(.trailing)
+        
+        options[0].align(.leading)
+        options[0].connectTop(to: textField)
+        options[0].connectRight(to: options[1])
+        
+        options[1].connectTop(to: textField)
+        options[1].connectRight(to: options[2])
+        
+        options[2].connectTop(to: textField)
+        options[2].align(.trailing)
+        
+        options[3].align(.leading)
+        options[3].connectRight(to: options[4])
+        options[3].align(.bottom)
+        
+    }
+    
+    func displayOptions (_ optionText : [String] = ["North", "West", "South", "East"]) {
+        clearOptions()
+        for i in 0..<(min(optionText.count, 4)) {
+            options[i].text = optionText[i]
+        }
+    }
+    
+    func clearOptions() {
+        options.forEach {option in option.text = ""}
+    }
+    
+    func didTapNorthButton() {
         if room.exits[.North] != nil {
             delegate?.moveToRoom(RoomDictionary[room.exits[.North]!]!)
         }
+    }
+    
+    func didTapWestButton() {
+        if room.exits[.West] != nil {
+            delegate?.moveToRoom(RoomDictionary[room.exits[.West]!]!)
+        }
+    }
+    
+    func didTapSouthButton() {
+        if room.exits[.South] != nil {
+            delegate?.moveToRoom(RoomDictionary[room.exits[.South]!]!)
+        }
+    }
+    
+    func didTapEastButton() {
+        if room.exits[.East] != nil {
+            delegate?.moveToRoom(RoomDictionary[room.exits[.East]!]!)
+        }
+    }
+    
+    func didTapRoomName(){
+        delegate?.didTapRoomName()
     }
     
 }
@@ -51,6 +112,7 @@ class RoomNameLabel : UILabel {
 }
 
 protocol RoomViewDelegate {
-    func didTapRoomName(_ room : Room)
+    func didTapRoomName()
     func moveToRoom(_ room : Room)
 }
+
--- LibraryEscape/RoomViewController.swift
+++ LibraryEscape/RoomViewController.swift
@@ -12,6 +12,7 @@ class RoomViewController: UIViewController {
     
     var room : Room
     var delegate : RoomViewControllerDelegate?
+    var roomView : RoomView?
     
     init(_ room : Room) {
         self.room = room
@@ -24,11 +25,78 @@ class RoomViewController: UIViewController {
         self.room = StudyRoomB()
         super.init(coder: aDecoder)
     }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        roomView = RoomView(room)
+        
+        let northTap = UITapGestureRecognizer(target: self, action: #selector(RoomViewController.northButtonTapped))
+        let westTap = UITapGestureRecognizer(target: self, action: #selector(RoomViewController.westButtonTapped))
+        let southTap = UITapGestureRecognizer(target: self, action: #selector(RoomViewController.southButtonTapped))
+        let eastTap = UITapGestureRecognizer(target: self, action: #selector(RoomViewController.eastButtonTapped))
+        
+        roomView?.options.forEach {option in
+            option.isUserInteractionEnabled = true
+        }
+        
+        roomView?.options[0].addGestureRecognizer(northTap)
+        roomView?.options[1].addGestureRecognizer(westTap)
+        roomView?.options[2].addGestureRecognizer(southTap)
+        roomView?.options[3].addGestureRecognizer(eastTap)
+        
+        
+        self.view = roomView!
+    }
+    
+    @objc func northButtonTapped () {
+        let text = roomView?.options[0].text
+        if text == "Look At" {
+            delegate?.optionWasTapped("North")
+        } else {
+            //??
+        }
+    }
+    
+    @objc func westButtonTapped () {
+        let text = roomView?.options[1].text
+        if text == "Look At" {
+            delegate?.optionWasTapped("West")
+        } else {
+            //??
+        }
+    }
+    
+    @objc func southButtonTapped () {
+        let text = roomView?.options[2].text
+        if text == "Look At" {
+            delegate?.optionWasTapped("South")
+        } else {
+            //??
+        }
+    }
+    
+    @objc func eastButtonTapped () {
+        let text = roomView?.options[3].text
+        if text == "Look At" {
+            delegate?.optionWasTapped("East")
+        } else {
+            //??
+        }
+    }
+    
+    //    func displayText (_ text : String) {
+    //        roomView?.displayText(text)
+    //    }
+    //
+    //    func displayOptions () {
+    //        roomView?.displayOptions()
+    //    }
+    
 }
 
 extension RoomViewController : RoomViewDelegate {
-    func didTapRoomName(_ room: Room) {
-        delegate?.didTapRoomName(room)
+    func didTapRoomName() {
+        delegate?.didTapRoomName()
     }
     
     func moveToRoom(_ room: Room) {
@@ -39,5 +107,8 @@ extension RoomViewController : RoomViewDelegate {
 }
 
 protocol RoomViewControllerDelegate : RoomViewDelegate {
+    func optionWasTapped (_ option : String)
+    //??
     
 }
+
